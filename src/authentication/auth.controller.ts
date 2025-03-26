83cfc6e1e2af5478b1f78f94979b616bb33f2eb7
Nta1e <ericebuluochol@gmail.com>
2025-03-26 23:43:12 +0300

    version register and login routes

--- src/authentication/auth.controller.ts
+++ src/authentication/auth.controller.ts
@@ -1,21 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
-import { LoginDto } from './dto/login.dto';
-import { RegisterDto } from './dto/register.dto';
-import { ApiResponse } from '../common/utils/api-response.util';
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { ApiTags } from "@nestjs/swagger";
+import { LoginDto } from "./dto/login.dto";
+import { RegisterDto } from "./dto/register.dto";
+import { ApiResponse } from "../common/utils/api-response.util";
 
-@ApiTags('Auth')
-@Controller('auth')
+@ApiTags("Auth")
+@Controller({ path: "auth", version: "1" })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
+  @Post("register")
   async register(@Body() registerDto: RegisterDto): Promise<ApiResponse<any>> {
     return this.authService.register(registerDto);
   }
 
-  @Post('login')
+  @Post("login")
   async login(@Body() loginDto: LoginDto): Promise<ApiResponse<any>> {
     return this.authService.login(loginDto);
   }

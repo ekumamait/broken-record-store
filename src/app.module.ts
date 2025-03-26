4db62a6990bf78465b94f1d448a517e76b956bcf
Nta1e <ericebuluochol@gmail.com>
2025-03-26 15:59:22 +0300

    update records functionality and introduce orders resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppConfig } from './app.config';
 import { RecordsModule } from './records/records.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(AppConfig.mongoUrl), RecordsModule],
+  imports: [
+    MongooseModule.forRoot(AppConfig.mongoUrl), 
+    RecordsModule,
+    OrdersModule
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './app.config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // Import Swagger
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,14 +13,37 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Record API')
     .setDescription('The record management API')
+    .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+
+  // Error handling middleware
+  app.use((err, _req, res, _next) => {
+    res.status(500).json({
+      error: 'INTERNAL_SERVER_ERROR',
+      message: err?.message,
+      success: false,
+    });
+  });
+
+  // wrong route
+  // app.use((req, res) =>
+  //   res.status(405).send({
+  //     status: 405,
+  //     error: 'NO_URL_FOUND',
+  //   })
+  // );
 
   await app.listen(AppConfig.port);
 }
